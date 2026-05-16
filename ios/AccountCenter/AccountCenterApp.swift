ccbb5e55d6a73a7630372496095a1a50d65498c3
sunxi <sunxi@example.com>
2026-05-16 09:09:31 +0800

    feat: Task1.1 iOS skeleton

---
+++ ios/AccountCenter/AccountCenterApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct AccountCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
---
+++ ios/AccountCenter/RootView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Sprint4: Coming soon!")
+            .font(.title)
+            .padding()
+    }
+}
+
+#Preview {
+    RootView()
+}
